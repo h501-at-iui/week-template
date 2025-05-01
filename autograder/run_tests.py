271f1d3a17ddfadd0bfca9d2d9ae3bbfc90839b1
leon todd johnson <leonjohn@iu.edu>
2025-05-01 12:32:55 -0400

    add output note to tests

--- autograder/run_tests.py
+++ autograder/run_tests.py
@@ -1,10 +1,21 @@
 import unittest
+import json
 from gradescope_utils.autograder_utils.json_test_runner import JSONTestRunner
 
 if __name__ == '__main__':
     suite = unittest.defaultTestLoader.discover('tests')
+
+    # Get a default output from the results file
+    output = '''
+    When you're ready, make sure to submit your code for review on Canvas!
+    '''
+
+    # Run the tests and write the results to /autograder/results/results.json
     with open('/autograder/results/results.json', 'w') as f:
-        JSONTestRunner(stream=f, 
-                       verbosity=2, 
-                       buffer=True, 
-                       visibility='visible').run(suite)
+        runner = JSONTestRunner(stream=f, 
+                                verbosity=2, 
+                                buffer=True, 
+                                visibility="visible")
+        
+        runner.json_data['output'] = output
+        runner.run(suite)
